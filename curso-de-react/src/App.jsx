058f97e2e5dae33f4088b4f57527d4898071e9db
willsonbs <willsonbs@gmail.com>
2025-01-29 22:11:53 -0300

    descartei alguns alterações

--- curso-de-react/src/App.jsx
+++ curso-de-react/src/App.jsx
@@ -1,79 +1,79 @@
-import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTasks";
-import { use } from "react";
+import { useState, useEffect } from "react"; 
+import { v4 as uuidv4 } from "uuid"; 
+import Tasks from "./components/Tasks"; 
+import AddTask from "./components/AddTasks"; 
+import { use } from "react"; 
+ 
+function App() { 
+// Carregar as tarefas do localStorage ao iniciar 
+const loadTasks = () => { 
+const savedTasks = localStorage.getItem("tasks"); 
+return savedTasks ? JSON.parse(savedTasks) : []; 
+}; 
+ 
+const [tasks, setTasks] = useState(loadTasks); 
+ 
+// Atualizar o localStorage sempre que tasks for alterado 
+useEffect(() => { 
+localStorage.setItem("tasks", JSON.stringify(tasks)); 
+}, [tasks]); 
+ 
+useEffect(() => { 
+// CHAMAR A API 
+const fetchTasks = async () => { 
+const response = await fetch( 
+"https://jsonplaceholder.typicode.com/todos?_limit=10", 
+{ 
+method: "GET", 
+} 
+); 
+//PEGAR OS DADOS QUE ELA RETORNA 
+const data = await response.json(); 
+ 
+// ARMAZENAR/PERSISTIR ESSES DADOS NO STATE 
+//setTasks(data); 
+ 
+}; 
+fetchTasks(); 
+}, []); 
+ 
+function onTaskClick(taskId) { 
+const newTasks = tasks.map((task) => 
+task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task 
+); 
+setTasks(newTasks); 
+} 
+ 
+function onDeleteTaskClick(taskId) { 
+const newTasks = tasks.filter((task) => task.id !== taskId); 
+setTasks(newTasks); 
+} 
+ 
+function onAddTaskSubmit(title, description) { 
+const newTask = { 
+id: uuidv4(), 
+title, 
+description, 
+isCompleted: false, 
+}; 
+setTasks((prevTasks) => [...prevTasks, newTask]); 
+} 
+ 
+return ( 
+<div className="w-screen h-screen bg-slate-900 flex justify-center p-6"> 
+<div className="w-[500px] space-y-4"> 
+<h1 className="text-4xl text-slate-100 font-bold text-center mb-5"> 
+Gerenciador de Tarefas 
+</h1> 
+<AddTask onAddTaskSubmit={onAddTaskSubmit} /> 
+<Tasks 
+tasks={tasks} 
+onTaskClick={onTaskClick} 
+onDeleteTaskClick={onDeleteTaskClick} 
+/> 
+</div> 
+</div> 
+); 
+} 
 
-function App() {
-  // Carregar as tarefas do localStorage ao iniciar
-  const loadTasks = () => {
-    const savedTasks = localStorage.getItem("tasks");
-    return savedTasks ? JSON.parse(savedTasks) : [];
-  };
-
-  const [tasks, setTasks] = useState(loadTasks);
-
-  // Atualizar o localStorage sempre que tasks for alterado
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  useEffect(() => {
-    // CHAMAR A API
-    const fetchTasks = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=10",
-        {
-          method: "GET",
-        }
-      );
-    //PEGAR OS DADOS QUE ELA RETORNA
-      const data = await response.json();
-
-    // ARMAZENAR/PERSISTIR ESSES DADOS NO STATE
-    //setTasks(data);
-
-    };
-    fetchTasks();
-  }, []);
-
-  function onTaskClick(taskId) {
-    const newTasks = tasks.map((task) =>
-      task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
-    );
-    setTasks(newTasks);
-  }
-
-  function onDeleteTaskClick(taskId) {
-    const newTasks = tasks.filter((task) => task.id !== taskId);
-    setTasks(newTasks);
-  }
-
-  function onAddTaskSubmit(title, description) {
-    const newTask = {
-      id: uuidv4(),
-      title,
-      description,
-      isCompleted: false,
-    };
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-  }
-
-  return (
-    <div className="w-screen h-screen bg-slate-900 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
-        <h1 className="text-4xl text-slate-100 font-bold text-center mb-5">
-          Gerenciador de Tarefas
-        </h1>
-        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
-        <Tasks
-          tasks={tasks}
-          onTaskClick={onTaskClick}
-          onDeleteTaskClick={onDeleteTaskClick}
-        />
-      </div>
-    </div>
-  );
-}
-
-export default App;
+export default App; 
